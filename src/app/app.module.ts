6a3e6f70d69b79c592c02c10128f48dcbdf7ceb1
justin clappsy <justinc9877@gmail.com>
2018-12-28 12:37:12 -0500

    Fixed imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { MyMusicComponent } from './my-music/my-music.component';
+import { HomeComponent } from './blogs/home/home.component';
+import { MyMusicComponent } from './blogs/my-music/my-music.component';
 import { AboutComponent } from './about/about.component';
-import { ContactArchiveComponent } from './contact-archive/contact-archive.component';
-import { ContactModalComponent } from './contact-modal/contact-modal.component';
-import { BlogArchiveComponent } from './blog-archive/blog-archive.component';
+import { ContactArchiveComponent } from './contact/contact-archive/contact-archive.component';
+import { ContactModalComponent } from './contact/contact-modal/contact-modal.component';
+import { BlogArchiveComponent } from './blogs/blog-archive/blog-archive.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    MyMusicComponent,
-    AboutComponent,
-    ContactArchiveComponent,
-    ContactModalComponent,
-    BlogArchiveComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		HomeComponent,
+		MyMusicComponent,
+		AboutComponent,
+		ContactArchiveComponent,
+		ContactModalComponent,
+		BlogArchiveComponent
+	],
+	imports: [ BrowserModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
